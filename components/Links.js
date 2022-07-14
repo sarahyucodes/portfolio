6f742351de3572a85dfeb448e0a3822dea07c9f6
Sarah Yu <sarahyu0692@gmail.com>
2022-07-13 22:14:44 -0400

    wip

--- components/Links.js
+++ components/Links.js
@@ -1,14 +1,22 @@
-export default function Links() {
+export default function Links({ links }) {
     return (
-        <section className='grid grid-cols-3 gap-x-2 py-10 md:grid-cols-4'>
+        <section className='grid grid-cols-3 gap-x-2 py-10 md:grid-cols-4 md:py-20'>
           <div className='col-span-1'>
             <h2 className='font-semibold'>Links</h2>
           </div>
           <div className='col-span-2 md:cols-span-3'>
             <ul>
-              <li><a href='mailto:sarahyu.work@gmail.com' target='_blank' className='text-blue-700 font-semibold underline decoration-2 hover:text-slate-900 transition'>Email</a></li>
-              <li><a href='https://www.linkedin.com/in/sarah---yu' target='_blank' className='text-blue-700 font-semibold underline decoration-2 hover:text-slate-900 transition'>LinkedIn</a></li>
-              <li><a href='https://drive.google.com/file/d/1YW7dZuptZRE0iRVncIeU1YttGwgZE6Vh/view?usp=sharing' target='_blank' className='text-blue-700 font-semibold underline decoration-2 hover:text-slate-900 transition'>Resume</a></li>
+                {
+                    links.map((link, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={link.url} target='_blank' className='text-blue-700 font-semibold underline decoration-2 hover:text-slate-900 transition'>
+                                    {link.title}
+                                </a>
+                            </li>
+                        )
+                    })
+                }
             </ul>
           </div>
         </section>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 //
 import Links from './../components/Links'
 
+const contactLinks = [
+  {
+    title: 'Email',
+    url: 'mailto:sarahyu.work@gmail.com'
+  },
+  {
+    title: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/sarah---yu'
+  },
+  {
+    title: 'Resume',
+    url: 'https://drive.google.com/file/d/1YW7dZuptZRE0iRVncIeU1YttGwgZE6Vh/view?usp=sharing'
+  }
+]
+
 export default function Home() {
+  const [links, setLinks] = useState([])
+
+  useEffect(() => {
+    setLinks(contactLinks)
+  }, [])
+
   return (
     <div className='Home'>
       <Head>
@@ -10,12 +32,12 @@ export default function Home() {
         <meta name="description" content="Sarah Yu is a front-end web developer based in New York." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='container mx-auto py-10 px-5 md:py-36 md:px-10'>
+      <main className='container max-w-screen-lg mx-auto py-10 px-5 md:py-36 md:px-10'>
         <h1 className='text-2xl'>
           Sarah Yu is a front-end <span className='whitespace-nowrap'>web developer </span><br />
           based in New York.
         </h1>
-        <Links />
+        <Links links={links} />
       </main>
     </div>
   )
