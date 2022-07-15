4d6d0111d0d53e548cc602f5a6db108c0c1e5967
Sarah Yu <sarahyu0692@gmail.com>
2022-07-15 16:57:25 -0400

    framer motion entry animation

--- components/Layout.js
+++ components/Layout.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import Head from 'next/Head'
 //
 import Container from './Container'
@@ -8,19 +9,36 @@ export default function Layout({
     title='Sarah Yu',
     content='Sarah Yu is a front-end web developer based in New York.'
 }) {
+    const variants = {
+        hidden: { opacity: 0, x: 0, y: 0 },
+        enter: { opacity: 1, x: 0, y: 0 },
+        exit: { opacity: 0, x: 0, y: 0 }
+    }
+
     return (
         <>
-            <Head>
-                <title>{title}</title>
-                <meta name='description' content={content} />
-                <link rel='icon' href='/favicon.ico' />
-            </Head>
-            <main className='min-h-screen'>
-                <Container>
-                    {children}
-                </Container>
-            </main>
-            <Footer />
+                <Head>
+                    <title>{title}</title>
+                    <meta name='description' content={content} />
+                    <link rel='icon' href='/favicon.ico' />
+                </Head>
+                <main className='min-h-screen'>
+                    <motion.main
+                        initial='hidden'
+                        animate='enter'
+                        exit='exit'
+                        variants={variants}
+                        transition={{ 
+                            type: 'tween',
+                            duration: 0.4,
+                        }}
+                    >
+                        <Container>
+                                {children}
+                        </Container>
+                    </motion.main>
+                </main>
+                <Footer />
         </>
     )
 }
