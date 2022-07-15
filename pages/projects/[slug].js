f9fe259aaf6838d7b98a79925c522905d3f0b406
Sarah Yu <sarahyu0692@gmail.com>
2022-07-15 15:00:03 -0400

    try fix build bug

--- pages/projects/[slug].js
+++ pages/projects/[slug].js
@@ -23,33 +23,37 @@ export default function Project({ project }) {
                 <span className='inline-block mr-2'>←</span> 
                 <span className='inline-block group-hover:underline group-hover:underline-offset-2 group-hover:decoration-2'>Back to home</span>
             </button>
-            <Section>
-                <div className='col-span-full'>
-                    <h1 className='text-2xl md:text-3xl'>{title}</h1>
-                    <div className='text-sm font-medium text-blue-700 pt-2'>{client ?? null}</div>
-                </div>
-                <div className='col-span-full'>
-                    <p className='text-xl md:text-2xl'>
-                        {description}
-                    </p>
-                </div>
-                <div className='col-span-full flex flex-wrap' aria-label='technologies'>
-                    {
-                        technologies?.map((technology, index) => {
-                            return (
-                                <div
-                                    key={index}
-                                    className='
-                                    px-2 py-1 mr-2 last:mr-0 rounded border border-slate-900
-                                    text-xs font-semibold text-slate-900
-                                    '>
-                                    {technology}
-                                </div>
-                            )
-                        })
-                    }
-                </div>
-            </Section>
+            {
+                project ? (
+                    <Section>
+                        <div className='col-span-full'>
+                            <h1 className='text-2xl md:text-3xl'>{title}</h1>
+                            <div className='text-sm font-medium text-blue-700 pt-2'>{client ?? null}</div>
+                        </div>
+                        <div className='col-span-full'>
+                            <p className='text-xl md:text-2xl'>
+                                {description}
+                            </p>
+                        </div>
+                        <div className='col-span-full flex flex-wrap' aria-label='technologies'>
+                            {
+                                technologies?.map((technology, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            className='
+                                            px-2 py-1 mr-2 last:mr-0 rounded border border-slate-900
+                                            text-xs font-semibold text-slate-900
+                                            '>
+                                            {technology}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    </Section>
+                ) : null
+            }
         </Container>
     )
 }
